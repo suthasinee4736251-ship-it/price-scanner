b8719edf2a2045d7dfa5c1357c1fb542aa0f2701
suthasinee4736251-ship-it <suthasinee4736251@gmail.com>
2026-02-08 12:10:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Best Value Calculator", layout="centered")
 
-with st.sidebar:
-    st.markdown("## ⚙️ Settings")
-
 # ---------- TRANSLATIONS ----------
 TEXT = {
     "en": {
@@ -151,6 +148,7 @@ if st.session_state.products:
         st.session_state.products.clear()
 else:
     st.info(t["empty"])
+
 
 
 
